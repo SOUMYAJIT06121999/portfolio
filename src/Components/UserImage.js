95a56ddbe1d92ffe0e2c742734cb324a60fb2479
SOUMYAJIT06121999 <soumyajitbhadra20@gmail.com>
2024-02-27 14:16:07 +0530

    PORTFOLIO-REACT

--- src/Components/UserImage.js
+++ src/Components/UserImage.js
@@ -15,7 +15,7 @@ const UserImage = () => {
     <h1>REACT.JS DEVELOPER.</h1>
     <div>
        <Link to="/project" className="btn">Projects</Link> 
-       <Link to="/contact" className="btn btn-light">Contact</Link> 
+       <Link to="/contact" className="btn btn-light" >Contact</Link> 
     </div>
     </div>
     </div>

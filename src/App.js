fa725964196f3a5fdc8589d359a93dfddb47bf13
SOUMYAJIT06121999 <soumyajitbhadra20@gmail.com>
2024-09-06 16:08:26 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
- import React from "react";
+import React from "react";
  import "./index.css";
  import Home from "./routes/Home";
  import Project from "./routes/Project"; 
  import About from "./routes/About";
  import Contact from "./routes/Contact";
 
- 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import { Route, Routes } from "react-router-dom";
 
  function App() {
   return(
-    <Router>
+    <>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/project" element={<Project/>} />
       <Route path="/about" element={<About/>} />
       <Route path="/contact" element={<Contact/>} />
     </Routes>
-    </Router>
-    )}
+    </>
+    
+  )
 
+ }
 export default App;
--- src/Components/WorkCardData.js
+++ src/Components/WorkCardData.js
@@ -13,13 +13,13 @@ const ProjectCardData = [
         imgsrc: pro2,
         title:"WEATHER-APP",
         text:"This project is a simple weather application built using React. It allows users to search for weather information by entering the name of a city. The application fetches real-time weather data from the OpenWeatherMap API and displays it to the user. The weather data includes temperature, humidity, and wind speed, along with an icon representing the current weather conditions.",
-        view: "https://github.com/SOUMYAJIT06121999/weather-app.git",
+        view: "https://taupe-cranachan-59cde5.netlify.app/",
     },
     {
         imgsrc: pro3,
         title:"AGE-CALCULATOR",
         text:"This project is a simple age calculator implemented using JavaScript. It allows users to input their birthday and calculates their age based on the current date. The calculator considers the year, month, and day to accurately determine the age in years, months, and days. The project demonstrates how to perform date calculations in JavaScript to achieve the desired functionality.",
-        view: "https://github.com/SOUMYAJIT06121999/Age-Calculator.git",
+        view: "https://soumyajit06121999.github.io/Age-Calculator/",
     },
 ];
 
